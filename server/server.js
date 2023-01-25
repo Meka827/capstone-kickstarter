78ca2ba816ef7e67a06fe641a8a059974ebe52db
Meka827 <javy.meka@gmail.com>
2023-01-25 16:54:46 -0600

    simple working backend

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import postgres from "postgres";
 import nodemon from "nodemon";
 import cors from "cors";
 
-const sql = postgres("postgres://localhost/");
+const sql = postgres("postgres://localhost/kickstarter");
 const app = express();
 
 //=============Middleware=======================================
@@ -12,6 +12,70 @@ app.use(express.static("./client"));
 app.use(cors());
 
 //==============Routes====================================
+
+app.get("/projects", (req, res) => {
+    sql`SELECT * FROM projects`.then(results => {
+        res.send(results)
+    })
+    console.log('is working')
+});
+
+app.get("/projects/:id", (req, res) => {
+    const id = req.params.id;
+    sql`SELECT * FROM projects WHERE id = ${id}`.then(results => {
+        if (results.length !== 0) {
+            res.json(results[0]);
+        } else {
+            res.status(404);
+            res.set("Content-Type", "text/plain");
+            res.send("Not Found");
+        };
+    })
+});
+
+//==============campaign Routes====================================
+
+app.get("/campaign", (req, res) => {
+    sql`SELECT * FROM campaign`.then(results => {
+        res.send(results)
+    })
+    console.log('is working')
+});
+
+app.get("/campaign/:id", (req, res) => {
+    const id = req.params.id;
+    sql`SELECT * FROM campaign WHERE id = ${id}`.then(results => {
+        if (results.length !== 0) {
+            res.json(results[0]);
+        } else {
+            res.status(404);
+            res.set("Content-Type", "text/plain");
+            res.send("Not Found");
+        };
+    })
+})
+
+//============== creator Routes====================================
+
+app.get("/creator", (req, res) => {
+    sql`SELECT * FROM creator`.then(results => {
+        res.send(results)
+    })
+    console.log('is working')
+});
+
+app.get("/creator/:id", (req, res) => {
+    const id = req.params.id;
+    sql`SELECT * FROM creator WHERE id = ${id}`.then(results => {
+        if (results.length !== 0) {
+            res.json(results[0]);
+        } else {
+            res.status(404);
+            res.set("Content-Type", "text/plain");
+            res.send("Not Found");
+        };
+    })
+})
 
 //==============ERROR HANDLERS==============================================
 app.use((err, req, res, mext) => {
