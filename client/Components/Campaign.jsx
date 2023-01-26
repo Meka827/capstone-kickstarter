d139fb7bdc18be12279b0ec0ee797d85648c0002
Spencer Coltharp <spencer.colth@gmail.com>
2023-01-26 11:19:25 -0600

    start of campagin .jsx

--- client/Components/Campaign.jsx
+++ client/Components/Campaign.jsx
@@ -1,1 +1,26 @@
+import React from "react";
+import { useRecoilState } from "recoil";
+import { campaignState } from "../state";
 
+const Campaign = () => {
+
+const [campaign, setCampaign] = useRecoilState(campaignState)   
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/campaign/${id}`, {
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCampaign(data);
+      });
+  }, []);
+
+
+  return (
+<div></div>
+  );
+
+};
+
+export default Campaign;
