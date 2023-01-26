3060c8ce8bdb781fe70b0ac7c15faed02afeccfb
Meka827 <javy.meka@gmail.com>
2023-01-26 13:48:40 -0600

    added Navbar.js

---
+++ client/Components/NavBar.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import Card from "./Card"
+// import Faq from './Faq'
+import Campaign from './Campaign'
+// import Community from './Community'
+// import Updates from './Updates'
+// import Comments from './Comments'
+
+
+
+const NavBar = () => {
+
+
+    
+    return (
+        <>
+            <Card></Card>
+          <nav className="nav-bar" >
+            <div className="menu" >
+                <div  className='menu-item' >
+                    <Link className="link" to="/campaign" activestyle="true">
+                        Campaign
+                    </Link>  
+                </div>
+                <div className='menu-item' >
+                    <Link className="link" to="/faq" activestyle="true">
+                        FAQ
+                    </Link>
+                </div>
+                <div className='menu-item' >
+                    <Link className="link" to="/updates" activestyle="true">
+                        Updates
+                    </Link>
+                </div>
+                <div className='menu-item' >
+                    <Link className="link" to="/comments" activestyle="true">
+                        Comments
+                    </Link>
+                </div>
+                <div className='menu-item' >
+                    <Link className="link" to="/community" activestyle="true">
+                        Community
+                    </Link>
+                </div>
+            </div>
+          </nav>
+        </>
+      );
+}
+
+export default NavBar;
