8858e054a7b695b345d9cb308159f87c4bb30612
kmnorth96 <kmnorth96@gmail.com>
2023-01-30 10:30:45 -0800

    Comments displayed on page. Still need to work on CSS

--- client/App.jsx
+++ client/App.jsx
@@ -1,61 +1,72 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route}from 'react-router-dom';
-import Faq from './components/Faq'
-import Campaign from './components/Campaign'
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { commentState } from "/state.js";
+import Faq from "./components/Faq";
+import Campaign from "./components/Campaign";
 // import Community from './components/Community'
 // import Updates from './components/Updates'
-// import Comments from './components/Comments'
-import NavBar from './components/NavBar';
-
-
-
+import Comments from "./components/Comments";
+import NavBar from "./components/NavBar";
 
 const App = () => {
-
-
-    useEffect(() => {
-        fetch("http://localhost:3000/projects", {
-        mode:"cors",
-       })
-       .then((res) => res.json())
-       .then(result => {
+  useEffect(() => {
+    fetch("http://localhost:3000/projects", {
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((result) => {
         console.log(result);
-       });
-    }, [])
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch("http://localhost:3000/campaign", {
-           mode:"cors",
-       })
-       .then((res) => res.json())
-       .then(result => {
+  useEffect(() => {
+    fetch("http://localhost:3000/campaign", {
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((result) => {
         console.log(result);
-       });
-    }, []) 
-    
-    useEffect(() => {
-        fetch("http://localhost:3000/creator", {
-           mode:"cors",
-       })
-       .then((res) => res.json())
-       .then(result => {
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/creator", {
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((result) => {
         console.log(result);
-       });
-    }, [])
+      });
+  }, []);
+
+  /*======================================COMMENTS===========================================*/
+  const [comments, setComments] = useRecoilState(commentState);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/comments", {
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((comments) => {
+        setComments(comments);
+      });
+  }, []);
+  /*======================================COMMENTS==========================================*/
 
   return (
     <>
-    <Router>
-    <Routes>
-        <Route path='/' element={<NavBar />} />
-        <Route path='/faq' element={<Faq />} />
-        {/* <Route path='/updates' element={<Updates />} /> */}
-        {/* <Route path='/comments' element={<Comments />} /> */}
-        {/* <Route path='/community' element={<Community />} /> */}
-    </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<NavBar />} />
+          <Route path="/faq" element={<Faq />} />
+          {/* <Route path='/updates' element={<Updates />} /> */}
+          <Route path="/comments" element={<Comments comments={comments} />} />
+          {/* <Route path='/community' element={<Community />} /> */}
+        </Routes>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
