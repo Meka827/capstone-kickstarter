78a6fef1c3f7e560884bfdc8bdcd2c9643ea08d6
kmnorth96 <kmnorth96@gmail.com>
2023-02-02 07:46:33 -0800

    Updated styling

--- client/Components/Comment.jsx
+++ client/Components/Comment.jsx
@@ -8,59 +8,75 @@ const Comments = () => {
 
   return (
     <>
-<NavBar />
-    <section className="comments-section">
-      <div className="py3">
-        <div className="container-flex">
-          <div className="row-comments">
-            <div className="col-right">
-              <div>
-                <p>
-                  <a>Have a question for the creator?</a>
-                </p>
-                <p></p>
-              </div>
-            </div>
-            <div className="col-left">
-              <div className="react-project-comments">
-                <p className="backers">
-                  Only backers can post comments.
-                  <a
-                    className="log"
-                    href="/login?ref=comments&then=https%3A%2F%2Fwww.kickstarter.com%2Fprojects%2F1172937197%2Fhero-realms-dungeons%2Fcomments"
-                  ></a>
-                </p>
-                <div>
-                  <button className="new-comment-btn" type="button">
-                    <span>Show 3 newer comments</span>
-                  </button>
+      <NavBar />
+      <section className="comments-section">
+        <div className="py3">
+          <div className="container-flex">
+            <div className="row-comments">
+              <div className="col-right">
+                <div className="border-left">
+                  <p className="type-14">
+                    This is your space to offer support and feedback. Remember
+                    to
+                    <a className="green"> be constructive</a> --there's a human
+                    behind this project.
+                  </p>
+                  <p>Have a question for the creator?</p>
+                  <div className="faq-link">
+                    <a>Check this project's FAQ</a>
+                  </div>
                 </div>
-                <ul className="comment-list">
-                  <li className="list-item">
-                    <div>
-                      <div></div>
-                      <div>
-                        <div>
-                          {comments.map((com) => {
-                            // console.log(com.comment);
-                            return (
+              </div>
+              <div className="col-left">
+                <div className="react-project-comments">
+                  <p className="backers">
+                    Only backers can post comments.
+                    <a className="log"> Log in</a>
+                  </p>
+                  {/* <div>
+                    <button className="new-comment-btn" type="button">
+                      <span>Show 3 newer comments</span>
+                    </button>
+                  </div> */}
+                  <ul className="comment-list">
+                    {/* {comments.map((com) => {
+                      return ( */}
+                    <li className="list-item">
+                      {comments.map((com) => {
+                        return (
+                          <div className="border-box">
+                            <div className="commentor-info">
+                              <div>
+                                <img className="image" src={com.image}></img>
+                                <div>
+                                  <span className="user-name">
+                                    <p className="user-name-text" key={com.id}>
+                                      {com.name}
+                                    </p>
+                                  </span>
+                                  <a></a>
+                                </div>
+                              </div>
+                            </div>
+                            <div className="comments">
                               <p className="data-comment-text" key={com.id}>
                                 {com.comment}
                               </p>
-                            );
-                          })}
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-                <div></div>
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </li>
+                    {/* );
+                    })} */}
+                  </ul>
+                  <div></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
