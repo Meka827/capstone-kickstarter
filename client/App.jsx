7d5b9e756a75aa6a082d51db5c6f30407fb26dec
Meka827 <javy.meka@gmail.com>
2023-02-15 15:09:59 -0600

    fixed route reload testing 2

--- client/App.jsx
+++ client/App.jsx
@@ -75,10 +75,10 @@ const App = () => {
     <Router>
     <Routes>
         <Route path='/' element={pledge.length !== 0 && campaign.length !== 0 && creator.length !== 0 && <Campaign pledge={pledge} campaign={campaign} creator={creator} />} />
-        <Route path='/faq' element={<Faq />} />
-        <Route path='/updates' element={<Updates />} />
-        <Route path='/comments' element={<Comments comments={comments} />} />
-        <Route path='/community' element={<Community />} />
+        <Route path='/#/faq' element={<Faq />} />
+        <Route path='/#/updates' element={<Updates />} />
+        <Route path='/#/comments' element={<Comments comments={comments} />} />
+        <Route path='/#/community' element={<Community />} />
     </Routes>
     </Router>
     </>
